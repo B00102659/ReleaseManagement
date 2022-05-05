2d730d3db2bc039a3b5798c317dbc5415bcded84
Jack GreeneByrne <greenebeen@pop-os.localdomain>
2022-05-05 11:02:12 +0100

    flask working

--- samplecode/server.py
+++ samplecode/server.py
@@ -5,21 +5,23 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-    
-    
+
+
 @app.route("/page2")
 def page2():
     return "<p>Hello this is the second page</p>"
-    
-    
-    
+
+
+
 @app.route("/page3")
 def page3():
     return "<p>Hello this is the third page</p>"
-    
 
 
-#example 6661    
+
+#example 6661
 @app.route("/getNumber")
 def page4():
     return "10"
+
+app.run()
